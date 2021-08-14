2fbdbebfa760f74418ee53120a521ee2246570cd
Ganesh Jadhav <gjadhav@deloitte.com>
2021-08-14 10:51:23 +0530

    initial commit

---
+++ randomnumgenerator/src/test/java/com/example/randomnumgenerator/RandomnumgeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.randomnumgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomnumgeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
